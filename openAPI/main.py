0045bf2b20d72747ccc3f6d93823822c3f204d73
chenwangsuchen <chensuchen@tp.edu.tw>
2024-01-19 15:46:48 +0800

    load_dotenv

--- openAPI/main.py
+++ openAPI/main.py
@@ -1,7 +1,11 @@
 from typing import Union
 
 from fastapi import FastAPI
-
+import redis
+import os
+from dotenv import load_dotenv
+load_dotenv()
+redis_conn = redis.Redis.from_url(os.environ.get('REDIS_HOST_PASSWORD'))
 app = FastAPI()
 
 
