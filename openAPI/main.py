7f4a95f8ff4bf82c749e34259cc9c9428b7d0185
chenwangsuchen <chensuchen@tp.edu.tw>
2024-01-19 16:07:00 +0800

    counter

--- openAPI/main.py
+++ openAPI/main.py
@@ -11,8 +11,13 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    counter = redis_conn.incr('test:increment',1)
+    return {"Counter": counter}
 
+@app.get("/counter/{c}")
+def counter(c:int):
+    counter = redis_conn.incr('test:increment',c)
+    return {"Counter": counter}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str | None = None):
