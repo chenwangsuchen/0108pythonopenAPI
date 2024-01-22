4a81cfbe0fd55a972f69fac7447f45238636481a
chenwangsuchen <chensuchen@tp.edu.tw>
2024-01-22 11:34:15 +0800

    modify

--- openAPI/main1.py
+++ openAPI/main1.py
@@ -23,3 +23,10 @@ async def get_item(date:str,celsius:float):
     print(f"日期:{date}")
     print(f"溫度:{celsius}")
     return {"日期":date,"攝氏溫度":celsius}
+
+
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
