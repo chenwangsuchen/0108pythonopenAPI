57f13e63fca61ed83ecf3638576c9f2adf2c98c4
chenwangsuchen <chensuchen@tp.edu.tw>
2024-01-24 12:08:54 +0800

    modify

--- openAPI/main.py
+++ openAPI/main.py
@@ -1,11 +1,11 @@
 from typing import Union
-
 from fastapi import FastAPI
 import redis
 import os
 from dotenv import load_dotenv
 load_dotenv()
 redis_conn = redis.Redis.from_url(os.environ.get('REDIS_HOST_PASSWORD'))
+
 app = FastAPI()
 
 
@@ -19,9 +19,11 @@ def counter(c:int):
     counter = redis_conn.incr('test:increment',c)
     return {"Counter": counter}
 
+
 @app.get("/pico_w/{date}")
-async def read_item(date:str ,address:str,celsius:float=0.0):
+async def read_item(date:str ,address:str,celsius:float,light:float):
     print(f"日期:{date}")
     print(f"位置:{address}")
     print(f"攝氏:{celsius}")
+    print(f"光線:{light}")
     return {"狀態":"儲存成功"}
