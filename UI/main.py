3f03862018dce255deb719d5c74bc2702469c378
chenwangsuchen <chensuchen@tp.edu.tw>
2024-01-26 11:37:47 +0800

    modify

---
+++ UI/main.py
@@ -0,0 +1,24 @@
+import requests
+import pandas as pd
+import streamlit as st
+
+url = 'https://openapi-lizv.onrender.com/pico_w/?count=10'
+
+r = requests.get(url=url)
+
+if r.status_code == 200:
+    print("下載成功")
+    data = r.json()
+
+dataFrame = pd.DataFrame(data)
+
+st.header("學院養雞場")
+st.divider()
+st.caption("溫度-光線表表😍")
+st.write(dataFrame)
+st.divider()
+st.caption("光線")
+st.line_chart(dataFrame,x='date',y='light')
+st.divider()
+st.caption("溫度")
+st.line_chart(dataFrame,x='date',y='temperature',color='#ff0000')
